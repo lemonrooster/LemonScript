bbdb0a3cf1f37e55de00519684b5bf45e7d4533f
lemonrooster <118646959+lemonrooster@users.noreply.github.com>
2023-12-01 10:43:37 -0500

    cmake-build-debug-wsl
    Add the above folder

---
+++ cmake-build-debug-wsl/CMakeFiles/lem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lem.dir/src/main.cpp.o"
+  "CMakeFiles/lem.dir/src/main.cpp.o.d"
+  "lem"
+  "lem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
